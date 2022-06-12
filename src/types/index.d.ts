a7a8e4d5f3e3150ae8c3fec334d3b26f86a7e82e
esllo <master@esllo.com>
2022-06-12 20:55:36 +0900

    add types

---
+++ src/types/index.d.ts
@@ -0,0 +1,31 @@
+import { AudioPlayer, AudioResource, VoiceConnection } from "@discordjs/voice";
+import { DiscordTogether } from "discord-together";
+import { BaseCommandInteraction, ChatInputApplicationCommandData, Client } from "discord.js";
+
+export type Nullable<T> = T | null;
+export type Nullish<T> = T | null | undefined;
+
+export interface CustomClient extends Client {
+  together?: DiscordTogether;
+  music?: any;
+}
+
+export interface Command extends ChatInputApplicationCommandData {
+  execute: (client: CustomClient, interaction: BaseCommandInteraction) => Promise<CommandResult>;
+}
+
+export type CommandResult = string | boolean | undefined | void;
+
+export interface Translation {
+  [key: string]: any;
+}
+
+export interface MusicData {
+  client: Nullable<CustomClient>;
+  conn: Nullable<VoiceConnection>;
+  connChannelId: string;
+  player: AudioPlayer;
+  resource: Nullable<AudioResource>;
+  stream: YouTubeStream;
+  queue: any[];
+}
